e312064fa1380b485da3ddc8ec953b7b2f47853f
Small Helm LLC <dev@smallhelm.com>
2015-02-14 13:37:23 -0700

    todomvc store items in a map not a vector

--- examples/todomvc/main.cljs
+++ examples/todomvc/main.cljs
@@ -3,14 +3,13 @@
             [hairball.vdom :as d :include-macros true]
             [hairball.app :refer [app-get app-swap!]]))
 
-(app-swap! [:items] [{:text "one"} {:done true  :text "two"}])
 (app-swap! [:selected-state] "All")
 
 (defn get-n-left []
-  (count (remove :done (app-get [:items]))))
+  (count (remove (fn [[id item]] (:done item)) (app-get [:items]))))
 
 (defn get-n-completed []
-  (count (filter :done (app-get [:items]))))
+  (count (filter (fn [[id item]] (:done item)) (app-get [:items]))))
 
 (defn drop-nth [n coll]
   (let [v (if (vector? coll)
@@ -19,24 +18,24 @@
     (vec (concat (subvec v 0 n) (subvec v (inc n) (count v))))))
 
 (defn clear-completed []
-  (app-swap! [:items] (into [] (remove :done (app-get [:items])))))
+  (app-swap! [:items] (into {} (remove (fn [[id item]] (:done item)) (app-get [:items])))))
 
-(defn onclick-rm-item [i]
+(defn onclick-rm-item [id]
   (fn []
-    (app-swap! [:items] (drop-nth i (app-get [:items])))))
+    (app-swap! [:items] (dissoc (app-get [:items]) id))))
 
 (defn add-new-item []
   (do
-    (app-swap! [:items] (conj (app-get [:items]) {:text (app-get [:new-item])}))
+    (app-swap! [:items] (assoc (app-get [:items]) (gensym) {:text (app-get [:new-item])}))
     (app-swap! [:new-item] "")))
 
-(defn Item [i _]
-  (d/li {:class (if (app-get [:items i :done]) "completed")}
+(defn Item [[id _]]
+  (d/li {:class (if (app-get [:items id :done]) "completed")}
         (d/div {:class "view"}
-               (d/Input [:items i :done] "checkbox" {:class "toggle"})
-               (d/label (app-get [:items i :text]))
+               (d/Input [:items id :done] "checkbox" {:class "toggle"})
+               (d/label (app-get [:items id :text]))
                (d/button {:class "destroy"
-                          :on-click (onclick-rm-item i)}))))
+                          :on-click (onclick-rm-item id)}))))
 
 
 (defn App []
@@ -55,13 +54,12 @@
                                   (d/Input [:toggle-all] "checkbox" {:class "cssid-toggle-all"})
                                   (d/label {:for "toggle-all"} "Mark all as complete")
                                   (d/ul {:class "cssid-todo-list"}
-                                        (map-indexed Item
-                                                     (filterv (fn [item]
-                                                                (case (app-get [:selected-state])
-                                                                  "Active"    (not (:done item))
-                                                                  "Completed" (:done item)
-                                                                  true))
-                                                              (app-get [:items])))))
+                                        (map Item (filter (fn [[id item]]
+                                                            (case (app-get [:selected-state])
+                                                              "Active"    (not (:done item))
+                                                              "Completed" (:done item)
+                                                              true))
+                                                          (app-get [:items])))))
                        (d/footer {:class "cssid-footer"}
                                  (d/span {:class "cssid-todo-count"}
                                          (d/strong (get-n-left)) " items left")
